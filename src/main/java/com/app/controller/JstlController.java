1ad42069d48303fe1d5bdcb4b2caa7eeb81f9bbd
starble25 <starble25@gmail.com>
2025-01-15 15:30:40 +0900

    redirect, forward 개념 및 연습

--- src/main/java/com/app/controller/JstlController.java
+++ src/main/java/com/app/controller/JstlController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.app.dto.DrinkItem;
@@ -16,15 +17,15 @@ public class JstlController {
 
 	@GetMapping("/jstl1")
 	public String jstl1(Model model) {
-
+		
 		model.addAttribute("msg", "이것은 메시지다");
-
+		
 		DrinkItem d1 = new DrinkItem();
 		d1.setItem("아메리카노");
 		d1.setType("커피");
-
+		
 		model.addAttribute("d1", d1);
-
+		
 		List<DrinkItem> drinkList = new ArrayList<DrinkItem>();
 		drinkList.add(new DrinkItem("카페라떼", "커피"));
 		drinkList.add(new DrinkItem("카페모카", "커피"));
@@ -32,36 +33,36 @@ public class JstlController {
 		drinkList.add(new DrinkItem("아이스티", "차"));
 		drinkList.add(new DrinkItem("레몬티", "차"));
 		drinkList.add(new DrinkItem("얼그레이티", "차"));
-
+		
 		model.addAttribute("drinkList", drinkList);
 		model.addAttribute("listSize", drinkList.size());
-
+		
 		//  ${listSize}
-
+		
 		// userType : "user" "admin"
 		model.addAttribute("userType", "admin");  
-
+		
 		model.addAttribute("isLogin", true);	//true O  false X
-
+		
 		return "jstl/jstl1";
 	}
-
+	
 	@GetMapping("/jstl2")
 	public String jstl2(Model model) {
-
+		
 		//  안녕하세요~?
 		//  안녕하세요~? (*빨간글자)
 		//  <span class='fontred'>안녕하세요~?</span>
-
+		
 		model.addAttribute("msg1", "<부등호>");
 		model.addAttribute("msg2", "&lt;부등호&gt;");
-
-
+		
+		
 		model.addAttribute("msgXml", "<script>alert('경고')</script>");
-
+		
 		return "jstl/jstl2";
 	}
-
+	
 	
 	//  /jstl/listTest?type=str
 	//  /jstl/listTest?type=member
@@ -93,6 +94,7 @@ public class JstlController {
 		model.addAttribute("memberList", memberList);
 		*/
 		
+		 */
 		//2) 각 type 파라미터에 따라 경우에 맞는 작업만 수행
 		/*
 		model.addAttribute("type", type);
@@ -121,6 +123,7 @@ public class JstlController {
 		return "jstl/listTest";
 		*/
 		
+		
 		//3) type별로 아예 페이지 자체를 나누어처리 하는 경우
 		if(type.equals("str")) {
 			model.addAttribute("msg", "스트링 리스트입니다.");
@@ -134,6 +137,7 @@ public class JstlController {
 			return "jstl/str";
 		} else {		
 		//if(type.equals("member")) {
+			//if(type.equals("member")) {
 			List<Member> memberList = new ArrayList<Member>();
 			
 			for(int i=1; i<=5; i++) {
@@ -147,6 +151,46 @@ public class JstlController {
 		
 		//return "jstl/listTest"; //잘못 접근 시 보여줄 페이지
 	}
+	@GetMapping("/jstl/listTest/{type}")
+	public String listTestPath(@PathVariable String type, Model model) {
+		if(type.equals("str")) {
+			model.addAttribute("msg", "스트링 리스트입니다.");
+			List<String> msgList = new ArrayList<String>();
+			for(int i=1; i<=15; i++)
+				msgList.add("스트링 리스트입니다.");
+			model.addAttribute("msgList", msgList);
+			return "jstl/str";
+		} else {		
+			//if(type.equals("member")) {
+			List<Member> memberList = new ArrayList<Member>();
+			for(int i=1; i<=5; i++) {
+				memberList.add(new Member("아이디"+i, "비번"+i, "이름"+i));
+			}
+			model.addAttribute("memberList", memberList);
+			return "jstl/member";
+		}
+	}
+
 	
-	
+	/*
+	@GetMapping("/jstl/listTest/str")
+	public String listTestStr(Model model) {
+		model.addAttribute("msg", "스트링 리스트입니다.");
+		List<String> msgList = new ArrayList<String>();
+		for(int i=1; i<=15; i++)
+			msgList.add("스트링 리스트입니다.");
+		model.addAttribute("msgList", msgList);
+		return "jstl/str";
+	}
+	@GetMapping("/jstl/listTest/member")
+	public String listTestMember(Model model) {
+		List<Member> memberList = new ArrayList<Member>();
+		for(int i=1; i<=5; i++) {
+			memberList.add(new Member("아이디"+i, "비번"+i, "이름"+i));
+		}
+		model.addAttribute("memberList", memberList);
+		return "jstl/member";
+		//return "jstl/listTest"; //잘못 접근 시 보여줄 페이지
+	}
+	 */
 }
---
+++ src/main/java/com/app/controller/RedirectController.java
@@ -0,0 +1,82 @@
+package com.app.controller;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import com.app.dto.Member;
+@Controller
+public class RedirectController {
+	@GetMapping("/re/re1")
+	public String re1() {
+		
+		return "re/re1";
+	}
+	
+	@GetMapping("/re/re2")
+	public String re2(HttpServletRequest request) {
+		System.out.println(request.getParameter("msg"));
+		
+		//로직
+		//request.setAttribute("msg", "re2 직접넣은 msg");
+		
+		return "re/re2";
+	}
+	
+	@GetMapping("/re/re3")
+	public String re3(HttpServletRequest request) {
+		
+		System.out.println(request.getParameter("msg"));
+		
+		return "re/re2";		//요청 re3 -> view re2
+//		return "re/re3";		//요청 re3 -> view re3 (redirect)
+	}
+	
+	@GetMapping("/re/re4")
+	public String re4(HttpServletRequest request, RedirectAttributes ra) {
+		//return "re/re2";  //view 경로     ..../re2.jsp
+		System.out.println(request.getParameter("msg"));
+		
+		request.setAttribute("msg", "re4 redirect msg");//redirect시 안넘어감
+		ra.addAttribute("msg", "re4 ra msg"); //파라미터 넘기기 
+		
+		//return "redirect:/re/re2"; //그 경로로 다시 요청! 요청할 주소!
+		//return "redirect:/re/re2?msg=re4 redirect msg"; //경로에 파라미터 추가
+		return "redirect:/re/re2";
+		
+		//return "/WEB-INF/views/re/re2.jsp";
+		//return "redirect:/re/re2";
+	}
+	
+	@GetMapping("/re/re5")
+	public String re5(HttpServletRequest request) {
+		System.out.println(request.getParameter("msg"));
+		
+		request.setAttribute("msg", "re5 forward msg");
+		
+		return "forward:/re/re2";
+	}
+	
+	
+	//연습
+	@GetMapping("/re/listTest")
+	public String listTest(@RequestParam String type) {
+		
+		if(type.equals("str")) {
+			return "redirect:/jstl/listTest/str";
+		} else {  //memeber
+			return "forward:/jstl/listTest/member";
+			/*
+			List<Member> memberList = new ArrayList<Member>();
+			for(int i=1; i<=5; i++) {
+				memberList.add(new Member("아이디"+i, "비번"+i, "이름"+i));
+			}
+			model.addAttribute("memberList", memberList);
+			return "jstl/member";
+			*/
+		}
+	}
+
+}
