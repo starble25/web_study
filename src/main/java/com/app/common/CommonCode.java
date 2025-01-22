48e5ea9ce0ccd71ea1ef7383e0000fc41cb539eb
starble25 <starble25@gmail.com>
2025-01-22 13:50:42 +0900

    공통코드 상수화 인터페이스에 정리

---
+++ src/main/java/com/app/common/CommonCode.java
@@ -0,0 +1,14 @@
+package com.app.common;
+
+public interface CommonCode {
+	//서비스내에서 사용하는 모든 상수형 코드들을 모아놓은 파일
+	
+	//User
+	public String USER_USERTYPE_CUSTOMER = "CUS";
+	public String USER_USERTYPE_ADMIN = "ADM";
+	
+	//Room
+	public String ROOM_VIEWTYPE_OCEAN = "OCN";
+	public String ROOM_VIEWTYPE_CITY = "CTY";
+	public String ROOM_VIEWTYPE_MOUNTAIN = "MOT";
+}
