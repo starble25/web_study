29f65f16d81680dac161c9daf0b99ee33ea814e7
starble25 <starble25@gmail.com>
2025-02-28 17:23:23 +0900

    리액트 스프링 로그인 처리

--- src/main/java/com/app/controller/study/ReactAPIController.java
+++ src/main/java/com/app/controller/study/ReactAPIController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.dto.study.DrinkItem;
+import com.app.dto.study.Member;
+import com.app.util.JwtProvider;
+import com.app.util.LoginManager;
 
 @RestController
 public class ReactAPIController {
@@ -76,6 +79,65 @@ public class ReactAPIController {
 		drinkList.add(new DrinkItem("item" + num, "type" + num));
 		
 		return drinkList;
+	}
+	
+	@PostMapping("/api/login")
+	public String login(@RequestBody Member member, HttpServletRequest request) {
+		System.out.println(member.getId());
+		System.out.println(member.getPw());
+		
+		//id pw -> DB select 비교
+		//성공 or 실패
+		
+		//로그인 성공으로 간주
+		LoginManager.setSessionLogin(request, member.getId());
+		//member.getId() 로그인 성공해서 session id 저장
+		
+		return "ok";
+	}
+	
+	@PostMapping("/api/loginCheck")
+	public String loginCheck(HttpServletRequest request) {
+
+		if( LoginManager.isLogin(request) ) {
+			String loginId = LoginManager.getLoginUserId(request);
+			System.out.println(loginId);
+			return loginId + " login";
+		} else {
+			return "Not Login";
+		}
+	}
+	
+	
+	@PostMapping("/api/loginJWT")
+	public String loginJWT(@RequestBody Member member, HttpServletRequest request) {
+		
+		//member 값 DB 비교 -> 로그인 정상 여부..
+		//로그인 성공 간주
+		//JWT AccessToken 발행 -> FE 전달
+		String accessToken = JwtProvider.createAccessToken(member.getId());
+		System.out.println("로그인아이디 : " + member.getId());
+		System.out.println("발생 accessToken" + accessToken);
+		
+		return accessToken;
+	}
+	
+	@PostMapping("/api/loginCheckJWT")
+	public String loginCheckJWT(HttpServletRequest request) {
+		
+		//token 열어보고 -> 유효한 토큰인지 -> 누가 로그인했는지 -> 로직실행(DB정보조회) -> return
+		String accessToken = JwtProvider.extractToken(request);
+		if( accessToken == null ) { //인증없음
+			return "no"; // ApiResponse
+		}
+		System.out.println("accessToken : " + accessToken);
+		System.out.println(JwtProvider.isVaildToken(accessToken));
+		if( JwtProvider.isVaildToken(accessToken) ) {
+			String userId = JwtProvider.getUserIdFromToken(accessToken);
+			System.out.println("토큰에 들어있는 id : " + userId);
+		}
+		
+		return "ok";
 	}
 	
 }
